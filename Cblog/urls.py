3e04d9d47062f28104b83b728a2330b751fcfdae
Crescens <2e3crescens@gmail.com>
2022-02-10 12:42:38 +0800

    hello world created in cblogapp

--- Cblog/urls.py
+++ Cblog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cblogapp.views import myView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        path('cblogapp/', myView),
 ]
